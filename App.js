c49d6cfef823d016adb5b01f210c27f94c8d08b2
AntonMiranovich <antonmir992@gmail.com>
2024-05-14 23:36:26 +0300

    start project

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { gStyle } from './styles/style';
+import Main from './components/Main'
+
+export default function App() {
+  return (
+    <Main />
+  );
+
+}
+
+const styles = StyleSheet.create({
+
+});
